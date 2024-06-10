2bf7244de70fbe05bfe5869610d67aef8b1c0c77
Soumi-Paul-dev-99 <soumi.paul.dev99@gmail.com>
2024-06-10 16:48:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+console.log(`random number: ${generateRandomNumber()}`);
+
+console.log(`celcius To Farenhite : ${celciusToFahrenheit(0)}`);
